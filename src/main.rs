525969a6cb6c2d76665f68aa4c5a17e0168c10f3
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2025-11-22 09:19:51 +0100

    compute Scaled Jacobians

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::f32::MIN;
 use std::fs;
+use std::ops::{Div, Sub};
 
 const INPUT_FILE: &str = "input.mesh";
 
 // MEDIT .mesh format for hex-meshes
+// /!\ in hexahedra definitions, vertex indices are 1-based
 // ---
 // MeshVersionFormatted 2
 // Dimension 3
@@ -18,36 +21,136 @@ const INPUT_FILE: &str = "input.mesh";
 // 
 // End
 
+fn det2x2(a11: &f32, a12: &f32, a21: &f32, a22: &f32) -> f32 {
+    a11*a22-a12*a21
+}
+
 #[derive(Debug, Clone, Copy)]
-struct Point3D {
+struct Vec3 {
     x: f32,
     y: f32,
     z: f32
 }
 
+fn dot(v0: &Vec3, v1: &Vec3) -> f32 {
+    v0.x * v1.x + v0.y * v1.y + v0.z * v1.z
+}
+
+fn cross(v0: &Vec3, v1: &Vec3) -> Vec3 {
+    Vec3 {
+        x: det2x2(&v0.y, &v1.y, &v0.z, &v1.z),
+        y: det2x2(&v0.z, &v1.z, &v0.x, &v1.x),
+        z: det2x2(&v0.x, &v1.x, &v0.y, &v1.y)
+    }
+}
+
+impl Vec3 {
+    const ZERO: Vec3 = Vec3 { x: 0.0, y: 0.0, z: 0.0 };
+
+    fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x: x, y: y, z: z}
+    }
+
+    fn length(&self) -> f32 {
+        return (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+    }
+
+    fn normalized(self) -> Vec3 {
+        self / self.length()
+    }
+
+    fn dot(self, rhs: &Vec3) -> f32 {
+        dot(&self,rhs)
+    }
+
+    fn cross(self, rhs: &Vec3) -> Vec3 {
+        cross(&self,rhs)
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec3 { x: self.x / rhs, y: self.y / rhs, z: self.z / rhs}
+    }
+}
+
+impl Sub<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Vec3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z}
+    }
+}
+
+// https://github.com/LIHPC-Computational-Geometry/validity-first-polycube-labeling/blob/main/include/geometry_hexahedra.h
+// MEDIT convention (.mesh files)
+//        5-------6
+//       /|      /|
+//      / |     / |
+//     1-------2  |
+//     |  4----|--7
+//     | /     | /
+//     |/      |/
+//     0-------3
+static HEX_CORNER_SPLITING: [[usize; 4]; 8] = [
+    // corner index, then its 3 ordered neighbors
+    [0,3,4,1],
+    [1,5,2,0],
+    [2,1,6,3],
+    [3,7,0,2],
+    [4,0,7,5],
+    [5,6,1,4],
+    [6,2,5,7],
+    [7,4,3,6]
+];
+
 struct Hexahedra {
-    //   MEDIT convention (.mesh files)
-    //        5-------6
-    //       /|      /|
-    //      / |     / |
-    //     1-------2  |
-    //     |  4----|--7
-    //     | /     | /
-    //     |/      |/
-    //     0-------3
-    vertices: [usize; 8] // 8 indices, for 8 vertices
+    vertices: [usize; 8], // 8 indices, for 8 vertices
+}
+
+impl Hexahedra {
+    fn new(vertices: [usize; 8]) -> Hexahedra {
+        Hexahedra { vertices: vertices }
+    }
 }
 
 struct HexMesh {
-    points: Vec<Point3D>,
-    cells: Vec<Hexahedra>
+    points: Vec<Vec3>,
+    cells: Vec<Hexahedra>,
+    scaled_jacobians: Option<Vec<f32>>
 }
 
 impl HexMesh {
     pub fn new() -> HexMesh {
         let points = Vec::new();
         let cells = Vec::new();
-        HexMesh { points: points, cells: cells }
+        HexMesh { points: points, cells: cells, scaled_jacobians: None }
+    }
+
+    fn compute_scaled_jacobian(&self) {
+        let mut all_values: Vec<f32> = Vec::new();
+        all_values.resize(self.cells.len(),0.0); // fill with 0.0, self.cells.len() times
+        for hex_index in 0..self.cells.len() { // for each cell (each hexahedron)
+            let mut scaled_jacobian: f32 = 1.0;
+            for hex_corner in 0..8 { // for each of the 8 vertices of the current hexahedron
+                let mut v: [Vec3; 4] = [Vec3::ZERO, Vec3::ZERO, Vec3::ZERO, Vec3::ZERO];
+                for i in 0..4 { // [0] will be the current vertex, and [1:3] its 3 neighboring corners
+                    let which_corner = HEX_CORNER_SPLITING[hex_corner][i];
+                    let vertex_index = self.cells.get(hex_index).unwrap().vertices[which_corner];
+                    v[i] = *self.points.get(vertex_index-1).unwrap_or_else(|| {
+                            panic!("Cannot access `points` vec at {vertex_index}");
+                        }
+                    ); // get 3D coordinates of vertex at vertex_index
+                }
+                let n1: Vec3 = (v[1] - v[0]).normalized();
+                let n2: Vec3 = (v[2] - v[0]).normalized();
+                let n3: Vec3 = (v[3] - v[0]).normalized();
+                scaled_jacobian = f32::min(scaled_jacobian,dot(&n3,&cross(&n1,&n2)));
+            }
+            *all_values.get_mut(hex_index).unwrap() = scaled_jacobian; // update vector
+        }
     }
 }
 
@@ -90,11 +193,10 @@ fn main() {
                     else {
                         let parts: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
                         assert!(parts.len() == 4); // 4 parts: x, y, z and the value 1
-                        // let current_point: &mut Point3D = mesh.points.get_mut(vertices_counter).unwrap();
                         let x: f32 = parts[0].to_string().parse::<f32>().unwrap();
                         let y: f32 = parts[1].to_string().parse::<f32>().unwrap();
                         let z: f32 = parts[2].to_string().parse::<f32>().unwrap();
-                        mesh.points.push(Point3D{ x: x, y: y, z: z });
+                        mesh.points.push(Vec3{ x: x, y: y, z: z });
                     }
                     continue;
                 },
@@ -127,7 +229,7 @@ fn main() {
                         let v5: usize = parts[5].to_string().parse::<usize>().unwrap();
                         let v6: usize = parts[6].to_string().parse::<usize>().unwrap();
                         let v7: usize = parts[7].to_string().parse::<usize>().unwrap();
-                        mesh.cells.push(Hexahedra { vertices: [v0,v1,v2,v3,v4,v5,v6,v7] });
+                        mesh.cells.push(Hexahedra::new([v0,v1,v2,v3,v4,v5,v6,v7]));
                     }
                     continue;
                 },
@@ -138,5 +240,7 @@ fn main() {
         }
         println!("mesh.points.len() = {}", mesh.points.len());
         println!("mesh.cells.len() = {}", mesh.cells.len());
+        mesh.compute_scaled_jacobian();
+        println!("Scaled Jacobians computed");
     }
 }
